f2816a5e7053f09ef7d7b7ae59980c1c3e6f6110
Subraz Thapa <104885874+hecticSubraz@users.noreply.github.com>
2025-06-02 16:10:52 +0545

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,3 +1,21 @@
 function openSite(url) {
   chrome.tabs.create({ url: url });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btnNepse').addEventListener('click', () => {
+    openSite('https://nepsealpha.com/');
+  });
+
+  document.getElementById('btnTradingView').addEventListener('click', () => {
+    openSite('https://www.tradingview.com/');
+  });
+
+  document.getElementById('btnMeroLagani').addEventListener('click', () => {
+    openSite('https://merolagani.com/latestmarket.aspx');
+  });
+
+  document.getElementById('btnForbes').addEventListener('click', () => {
+    openSite('https://www.forbes.com/digital-assets/crypto-prices/?sh=77fb0c624785');
+  });
+});
